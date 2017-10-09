1c9712aae56830a3a6a7e2a2a7a3cfaa7c628c54
Sunitha Muthukrishna <mksunitha@gmail.com>
2017-10-09 13:17:40 -0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,29 @@
 
-echo "Starting SSH ..."
+#!/bin/bash
+log(){
+	while read line ; do
+		echo "`date '+%D %T'` $line"
+	done
+}
+
+set -e
+logfile=/home/LogFiles/entrypoint.log
+test ! -f $logfile && mkdir -p /home/LogFiles && touch $logfile
+exec > >(log | tee -ai $logfile)
+exec 2>&1
+
+set_var_if_null(){
+	local varname="$1"
+	if [ ! "${!varname:-}" ]; then
+		export "$varname"="$2"
+	fi
+}
+
+python --version
+pip --version
+
+echo "INFO: starting SSH ..."
 service ssh start
 
 echo "Running python" 
-python.exe /code/manage.py runserver 0.0.0.0:8000
+python /code/manage.py runserver 0.0.0.0:8000
